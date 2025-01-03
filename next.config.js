9acb607b2cce09a251ca1d86217dec995f084906
Todd W. <117058912+ToddW317@users.noreply.github.com>
2025-01-03 06:23:07 -0800

    t

--- next.config.js
+++ next.config.js
@@ -14,7 +14,11 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 }
 
 module.exports = nextConfig 
